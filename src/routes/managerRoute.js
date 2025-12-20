459bacd5a8963e6e4b0b7def44e58162952974bc
Anshu Mehra <dm0068726@gmail.com>
2025-12-20 21:50:59 +0530

    Add login route for manager

--- src/routes/managerRoute.js
+++ src/routes/managerRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { managerApplying } from "../controllers/managerController.js";
+import { managerApplying, managerLogin } from "../controllers/managerController.js";
 
 const router = express.Router();
 
 //Route for manager applying
 router.post("/apply", managerApplying);
+router.post("/login", managerLogin);
 
 export default router;
