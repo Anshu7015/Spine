41b95fe4f9f148dd4dd47474d83a24b1623531e0
Anshu Mehra <dm0068726@email.com>
2025-12-23 15:43:38 +0530

    updated file import

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createAdmin, loginAdmin, managerApproving } from "../controllers/adminController";
+import { createAdmin, loginAdmin, managerApproving } from "../controllers/adminController.js";
 
 const router = express.Router();
 
